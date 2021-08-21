ec768693c5722f5fb32e1861fef4bfc92c442381
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-08-21 20:23:47 +0300

    Fixing errors in About us

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+'use strict'
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 100;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
