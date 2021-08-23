a99ed2b79dee3d16155c68f2225ca0fa9459eb9f
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-08-23 23:32:21 +0300

    adding pop up on buy button

--- JS/cart.js
+++ JS/cart.js
@@ -175,12 +175,43 @@ function saveToLocalStorage(){
 
 
 let cancel = document.getElementById('cancel');
-cancel.addEventListener('click', removeAllItems );
+cancel.addEventListener('click', buyAllItems );
 
-function removeAllItems(event) {
+function buyAllItems(event) {
   let clearTable = document.getElementById('tableBody');
   clearTable.textContent = "";
   localStorage.clear()
   itemArray = [];
   loadDataToCart();
+  window.location.href="store.html";
+}
+
+
+
+
+
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("buy");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
